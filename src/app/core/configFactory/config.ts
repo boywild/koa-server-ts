dc7a1af5479aabc901552f2efc8be8171efbb64f
chentian <chentian.ct@qq.com>
2021-10-10 19:41:05 +0800

    修改

--- src/app/core/configFactory/config.ts
+++ src/app/core/configFactory/config.ts
@@ -1,17 +1,19 @@
 import { get, has, merge, set } from 'lodash'
 
 export default class Config {
+  /**
+   * 读取的配置信息都会合并到store中存储
+   * @private
+   */
   private store = {}
 
   private _prefix: string
-  private envSuffix: string
-  private envKey: string
+  private _suffix: string
   private baseDir: string
 
   constructor() {
     this._prefix = 'NODE'
-    this.envSuffix = 'NODE'
-    this.envKey = 'NODE'
+    this._suffix = '_ENV'
     this.baseDir = process.cwd()
   }
 
@@ -19,6 +21,10 @@ export default class Config {
     this.baseDir = baseDir
   }
 
+  /**
+   * 读取单条配置信息
+   * @param key 键
+   */
   public getItem(key = ''): string | number | unknown | Record<string, unknown> {
     if (key) {
       return get(this.store, key, '')
@@ -31,14 +37,27 @@ export default class Config {
     return this.store
   }
 
+  /**
+   * 设置单条配置信息
+   * @param key 键
+   * @param value 值
+   */
   public setItem(key: string, value: unknown): void {
     set(this.store, key, value)
   }
 
+  /**
+   * 判断配置信息是否存在
+   * @param key
+   */
   public hasItem(key: string): boolean {
     return has(this.store, key)
   }
 
+  /**
+   * 从配置文件中读取配置信息并到store中存储
+   * @param {String|Array} filePath 配置文件绝对路径
+   */
   public getConfigFromFile(filePath: string | Array<string>): void {
     const setStore = (path: string) => {
       const conf = <Record<string, unknown>>require(path).default || {}
@@ -55,33 +74,36 @@ export default class Config {
     }
   }
 
+  /**
+   * 从ts对象中读取键值队到store中存储
+   * @param {Object} obj
+   */
   public getConfigFromObj(obj: Record<string, unknown>): void {
     this.store = merge(this.store, obj)
   }
 
+  /**
+   * 从node的环境变量中读取固定字符串开头(NODE_ENV)的配置信息合并到store中存储
+   */
   public getConfigFromEnv(): void {
     const envKeys = Object.keys(process.env)
     const envs = {
-      [this.envKey]: 'debug'
+      [this.prefix + this.suffix]: 'debug'
     }
     envKeys.forEach((key) => {
-      if (key.startsWith(this.prefix + this.envSuffix)) {
-        const parts = key.split('_')
-        if (parts.length === 2) {
-          set(envs, this.envKey, process.env[key])
-        } else if (parts.length > 2) {
-          let k = key.replace(`${this.prefix}_`, '')
-          k = k.replace('_', '.')
-          set(envs, k, process.env[key])
-        }
+      if (key.startsWith(this.prefix + this.suffix)) {
+        set(envs, key, process.env[key])
       }
     })
 
     this.store = merge(this.store, envs)
   }
 
+  /**
+   * 获取当前系统环境(NODE_ENV)
+   */
   public getEnv(): string {
-    const env = this.getItem(this.envKey)
+    const env = this.getItem(this.prefix + this.suffix)
     if (typeof env === 'string') {
       return env.toLowerCase()
     } else {
@@ -89,16 +111,42 @@ export default class Config {
     }
   }
 
+  /**
+   * 判断当前系统是否属于debug环境
+   */
   public isDebug(): boolean {
     return this.getEnv() === 'debug'
   }
 
+  /**
+   * 设置环境变量特定前缀
+   * @param {String} value
+   */
   public set prefix(value: string) {
     this._prefix = value
   }
 
+  /**
+   * 获取环境变量特定前缀
+   */
   public get prefix(): string {
     return this._prefix
   }
+
+  /**
+   * 设置环境变量特定后缀
+   * @param {String} value
+   */
+  public set suffix(value: string) {
+    this._suffix = value
+  }
+
+  /**
+   * 获取环境变量特定后缀
+   */
+  public get suffix(): string {
+    return this._suffix
+  }
+
 
 }
